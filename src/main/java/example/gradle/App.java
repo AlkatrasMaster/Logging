5145ae96ceec0613cc61f1678dda4df9789fb3f8
AlkatrasMaster <149463469+AlkatrasMaster@users.noreply.github.com>
2024-04-26 17:40:23 +0500

    Add files via upload

---
+++ src/main/java/example/gradle/App.java
@@ -0,0 +1,15 @@
+package example.gradle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.debug("Debug message is written in console");
+        logger.info("Info message is written to stdout.log");
+        logger.error("Error message is written to stderr.log");
+    }
+}
